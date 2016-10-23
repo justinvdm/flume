bcc1a3c599e331d94d5f23a2272dcc355ffd110a
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-23 21:27:56 +0200

    test reduce for promise support

--- tests/reduce.test.js
+++ tests/reduce.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import { create, input, reduce } from '..';
-import { capture } from './utils';
+import { capture, defer } from './utils';
 
 
 test('reduce', t => {
@@ -15,5 +15,23 @@ test('reduce', t => {
     .dispatch(src, 2)
     .dispatch(src, 3);
 
+  t.deepEqual(res, [12, 15]);
+});
+
+
+test('promise support', async t => {
+  const src = input();
+  const res = [];
+  const d = defer();
+
+  const graph = [src]
+    .concat(reduce(() => 10, (total, v) => Promise.resolve(total + v)))
+    .concat(capture(res));
+
+  create(graph)
+    .dispatch(src, 2)
+    .dispatch(src, 3, d.resolve);
+
+  await d.promise;
   t.deepEqual(res, [12, 15]);
 });
