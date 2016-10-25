faf77dd46c534c93e2530563db31423a84cb315c
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-25 08:31:31 +0200

    add dev and prd bundles

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import node from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+
+
+export default {
+  entry: 'src/index',
+  format: 'umd',
+  moduleName: 'flume',
+  plugins: [
+    node(),
+    commonjs()]
+};
