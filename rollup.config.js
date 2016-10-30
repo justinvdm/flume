6f72f1894e90f20217d86e41523e7df18284e518
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-30 10:56:41 +0200

    sourcemaps for dev and prd builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
 import node from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import uglify from 'rollup-plugin-uglify';
 
 
 export default {
   entry: 'index',
   format: 'umd',
   moduleName: 'flume',
-  plugins: [node(), commonjs()]
+  plugins: [
+    node(),
+    commonjs(),
+    process.env.NODE_ENV === 'production' && uglify({compress: {unsafe: true}})
+  ]
 };
