77a62c72916af3cc5c3488c10ab895691b4f40bf
justinvdm <justinvdm.0.1.0@gmail.com>
2017-02-20 08:09:21 +0200

    support source pairs reducers

--- src/reduce.js
+++ src/reduce.js
@@ -1,8 +1,10 @@
-export default function reduce(init, fn) {
+export default function reduce(init, reducer) {
   if (arguments.length < 2) {
-    fn = init;
+    reducer = init;
     init = retNull;
   }
+
+  reducer = parseReducer(reducer);
 
   return {
     init: init,
@@ -10,12 +12,50 @@ export default function reduce(init, fn) {
   };
 
   function reduceFn(state, v, opts) {
-    var res = fn(state, v, opts);
+    try{
+    var res = reducer(state, v, opts);
     return [res, res];
+    } catch(e) { console.log(e); }
   }
+}
+
+
+function parseReducer(obj) {
+  if (typeof obj == 'function') return obj;
+  if (Array.isArray(obj)) return caseSourceOfReducer(obj);
+  throw new Error('Unsupported reducer type given to reduce(): ' + typeof obj);
+}
+
+
+function caseSourceOfReducer(pairs) {
+  var defaultFn = identityProcessor;
+
+  if (typeof pairs[pairs.length - 1] == 'function') {
+    defaultFn = pairs[pairs.length - 1];
+    pairs = pairs.slice(0, -1);
+  }
+
+  return function caseSourceOfFn(state, v, opts) {
+    var source = opts.source;
+    var i = -1;
+    var n = pairs.length;
+    var pair;
+
+    while (++i < n) {
+      pair = pairs[i];
+      if (pair[0] === source) return pair[1](state, v, opts);
+    }
+
+    return defaultFn(state, v, opts);
+  };
 }
 
 
 function retNull() {
   return null;
+}
+
+
+function identityProcessor(state, v) {
+  return v;
 }
--- tests/reduce.test.js
+++ tests/reduce.test.js
@@ -51,3 +51,61 @@ test('promise support', async t => {
   await d.promise;
   t.deepEqual(res, [12, 15]);
 });
+
+
+test('source pairs reducer', t => {
+  const src1 = input();
+  const src2 = input();
+  const src3 = input();
+  const res = [];
+
+  const graph = [[src1, src2, src3]]
+    .concat(reduce(() => 0, [
+      [src1, (total, v) => total + v],
+      [src2, (total, v) => total - v]
+    ]))
+    .concat(capture(res));
+
+  create(graph)
+    .dispatch(src1, 2)
+    .dispatch(src2, 3)
+    .dispatch(src2, 8)
+    .dispatch(src1, 7)
+    .dispatch(src3, 9)
+    .dispatch(src1, 4);
+
+  t.deepEqual(res, [2, -1, -9, -2, 9, 13]);
+});
+
+
+test('source pairs reducer default', t => {
+  const src1 = input();
+  const src2 = input();
+  const src3 = input();
+  const res = [];
+
+  const graph = [[src1, src2, src3]]
+    .concat(reduce(() => 0, [
+      [src1, (total, v) => total + v],
+      [src2, (total, v) => total - v],
+      (total, v) => total * v
+    ]))
+    .concat(capture(res));
+
+  create(graph)
+    .dispatch(src1, 2)
+    .dispatch(src2, 3)
+    .dispatch(src2, 8)
+    .dispatch(src1, 7)
+    .dispatch(src3, 9)
+    .dispatch(src1, 4);
+
+  t.deepEqual(res, [2, -1, -9, -2, -18, -14]);
+});
+
+
+test('unsupported reducer type', t => {
+  t.throws(
+    () => create([input].concat(reduce(23))),
+    'Unsupported reducer type given to reduce(): number');
+});
